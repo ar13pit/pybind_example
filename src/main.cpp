762e39a63115f2cbac3f1c74d83fa66d34df15e7
Arpit Aggarwal <ar13pit@gmail.com>
2020-03-01 23:47:32 +0100

    feat: Add numpy matrix example

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <stdexcept>
 #include <pybind11/pybind11.h>
 
 namespace py = pybind11;
@@ -20,7 +22,40 @@ public:
     Beta(Person* p1_) : p1(p1_) {}
 };
 
+class Matrix {
+public:
+    Matrix(size_t rows, size_t cols) : m_rows(rows), m_cols(cols) {
+        m_data = new float[rows*cols];
+    }
+    float *data() { return m_data; }
+    size_t rows() const { return m_rows; }
+    size_t cols() const { return m_cols; }
 
+    void set(size_t num_frames, float* frames) {
+      if (num_frames != m_rows*m_cols)
+      {
+        throw std::runtime_error("Not equal to allotted memory");
+      }
+      m_data = frames;
+    }
+
+    void print() {
+      size_t i;
+      std::cout << "Matrix(";
+      for (i = 0; i < m_rows*m_cols - 1; i++)
+      {
+        std::cout << m_data[i] << ", ";
+      }
+      std::cout << m_data[i] << ")" << std::endl;
+    }
+
+private:
+    size_t m_rows, m_cols;
+    float *m_data;
+};
+
+
+// pybind11 code
 PYBIND11_MODULE(python_example, m) {
   py::class_<Person>(m, "Person")
       .def(py::init<int, int>())
@@ -35,5 +70,20 @@ PYBIND11_MODULE(python_example, m) {
   py::class_<Beta>(m, "Beta")
       .def(py::init<Person *>())
       .def_readwrite("p1", &Beta::p1);
+
+  py::class_<Matrix>(m, "Matrix")
+      .def(py::init<size_t, size_t>())
+      .def("set", [](Matrix &m, py::buffer b) {
+        py::buffer_info info = b.request();
+
+        if (info.ndim != 2)
+            throw std::runtime_error("Incompatible buffer dimensions");
+
+        std::cout << "Input buffer dimensions: " << info.ndim << std::endl;
+        std::cout << "Input buffer shape: (" << info.shape[0] << ", " << info.shape[1] << ")" << std::endl;
+
+        m.set(info.shape[0]*info.shape[1], static_cast<float *>(info.ptr));
+              })
+      .def("print", &Matrix::print);
 
 }
